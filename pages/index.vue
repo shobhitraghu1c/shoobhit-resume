2c78f0a629332e7b60dcd34a7e0ab36f8e590290
shobhit <shobhitforwork@gmail.com>
2025-04-09 19:49:21 +0530

    home page creaed one section

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,30 @@
 <script setup lang="ts">
 
 </script>
-
 <template>
+    <div class="px-[12%] w-full h-full">
+        <div class="flex flex-col md:flex-row justify-between bg-[#FFFFFF] w-full h-fit rounded-md pt-10 px-5 pb-10">
+<div class="w-[50%] h-full flex flex-col gap-4">
+    <div class="w-full h-full flex flex-col ">
+        <h1 class="text-3xl font-bold text-[#b49286]">
+        Hello,
+    </h1>
+    <h1 class="text-3xl font-bold text-[#b49286] ">
+        I am Shobhit
+    </h1>
+    </div>
+    <div class="text-[#4a4a4a] font-semibold text-lg">
+    lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. 
+    </div>
     <div>
-this is home page
+        <button class="bg-[#c78283] text-[#ffffff] p-2 rounded-md ">Get Portfolio</button>
+    </div>
+</div>
+<div class="w-[50%] flex flex-row justify-end">
+img div
+</div>
+
+        </div>
     </div>
 </template>
 
