42693468a7c2247a5e3bce74ddd3bdbd8c070e45
Shobhit <shobhitforwork@gmail.com>
2025-06-05 12:05:28 +0530

    add site config in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,20 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss'],
+  app: {
+    head: {
+      title: 'Shoobhit Resume',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      ],
+    },
+  },
+  ssr: false,
+  target: 'static',
+  nitro: {
+    prerender: {
+      routes: ['/'],
+    },
+  },
 })
